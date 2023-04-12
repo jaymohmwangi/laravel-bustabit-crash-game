39c9fc7ceb53be1b42775e3938d85dd70d507671
James Mwangi <jaymoh53@gmail.com>
2023-04-12 20:18:12 +0300

    Added GiveAway contract and repository

---
+++ app/Contracts/GiveawayContract.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Contracts;
+
+use App\Models\Giveaway;
+use Illuminate\Support\Collection;
+
+interface GiveawayContract extends BaseRepositoryContract
+{
+    /**
+     * Get the giveaways for a specific user.
+     *
+     * @param int $userId
+     * @return Collection
+     */
+    public function getGiveawaysByUser(int $userId): Collection;
+
+    /**
+     * Get the total amount of giveaways for a specific user.
+     *
+     * @param int $userId
+     * @return float
+     */
+    public function getTotalAmountByUser(int $userId): float;
+
+    /**
+     * Create a new giveaway record for a user.
+     *
+     * @param int $userId
+     * @param float $amount
+     * @return Giveaway
+     */
+    public function createGiveaway(int $userId, float $amount): Giveaway;
+}
---
+++ app/Repositories/EloquentGiveawayRepository.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Contracts\GiveawayContract;
+use App\Models\Giveaway;
+use Illuminate\Support\Collection;
+
+class EloquentGiveawayRepository extends EloquentBaseRepository implements GiveawayContract
+{
+    public function __construct(Giveaway $model)
+    {
+        parent::__construct($model);
+    }
+
+    /**
+     * Get the giveaways for a specific user.
+     *
+     * @param int $userId
+     * @return Collection
+     */
+    public function getGiveawaysByUser(int $userId): Collection
+    {
+        return $this->model->where('user_id', $userId)->get();
+    }
+
+    /**
+     * Get the total amount of giveaways for a specific user.
+     *
+     * @param int $userId
+     * @return float
+     */
+    public function getTotalAmountByUser(int $userId): float
+    {
+        return $this->model->where('user_id', $userId)->sum('amount');
+    }
+
+    /**
+     * Create a new giveaway record for a user.
+     *
+     * @param int $userId
+     * @param float $amount
+     * @return Giveaway
+     */
+    public function createGiveaway(int $userId, float $amount): Giveaway
+    {
+        return $this->model->create([
+            'user_id' => $userId,
+            'amount' => $amount,
+        ]);
+    }
+}
